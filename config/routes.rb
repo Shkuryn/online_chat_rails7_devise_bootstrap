90fb05d7614390a7a17b5d8f2ea85b05167773fc
igor <shkuryn@gmail.com>
2024-01-25 22:56:34 +0200

    added messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
+  # resources :rooms do
+  #   resources :messages
+  # end
   resources :rooms
+  resources :messages
   resources :users, :only => [:index]
 end
